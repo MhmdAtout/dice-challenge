56a55d50a678e317233e6f1e0d3bd4824f8db26e
MhmdAtout <mohamad.atout0@icloud.com>
2023-02-25 00:36:42 +0200

    adding if statments

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var random_number2 = Math.floor(Math.random() * 6) + 1;
 var random_image1 = "assets/images/dice" + random_number1 + ".png"
 var random_image2 = "assets/images/dice" + random_number2 + ".png"
 
-document.querySelectorAll("img")[0].setAttribute("src", randomImage1);
-document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
+document.querySelectorAll("img")[0].setAttribute("src", random_image1);
+document.querySelectorAll("img")[1].setAttribute("src", random_image2);
 
+(random_number1 > random_number2) ? title.innerText = "Player 1 wins :)" :
+(random_number1 < random_number2) ? title.innerText = "Player 2 wins :)" :
+title.innerText = "Draw :{"
