df8b07fc124461d66194bf21470375602904322c
MhmdAtout <mohamad.atout0@icloud.com>
2023-02-25 00:25:36 +0200

    assigning the random number to choose one of the pictures

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var title = document.getElementById('title');
 
 var random_number1 = Math.floor(Math.random() * 6) + 1;
 var random_number2 = Math.floor(Math.random() * 6) + 1;
+
+var random_image1 = "assets/images/dice" + random_number1 + ".png"
+var random_image2 = "assets/images/dice" + random_number2 + ".png"
