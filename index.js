00453bee8e62dc1a90108aab890ad542588e0ca3
MhmdAtout <mohamad.atout0@icloud.com>
2023-02-25 00:27:17 +0200

    changing src attribute of the image according to the random number

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var random_number2 = Math.floor(Math.random() * 6) + 1;
 
 var random_image1 = "assets/images/dice" + random_number1 + ".png"
 var random_image2 = "assets/images/dice" + random_number2 + ".png"
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImage1);
+document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
+
